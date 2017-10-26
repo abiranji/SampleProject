48a19415f2315ba51eee9a1a365574fb769fafec
abiranji <abivaratharajan1994@gmail.com>
2017-10-26 21:23:38 +0530

    Add files via upload

---
+++ static.java
@@ -0,0 +1,9 @@
+class static {
+  public static void main(String[] args) {
+    display();
+  }
+ 
+  static void display() {
+    System.out.println("Java is my favorite programming language.");
+  }
+}
---
+++ static2.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+ 
+class Another {
+  public static void main(String[] args) {
+    int result;
+ 
+    result = Math.min(10, 20); 
+ 
+    System.out.println(result);
+    System.out.println(Math.max(100, 200));
+  }
+}
