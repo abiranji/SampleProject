59b0493d8b15e35b7cd14183e926b15341dbd8bc
abiranji <abivaratharajan1994@gmail.com>
2017-11-02 22:46:21 +0530

    Add files via upload

---
+++ MyArrayFill.java
@@ -0,0 +1,13 @@
+
+ 
+public class MyArrayFill {
+     
+    public static void main(String a[]){
+         
+        String[] myArr = new String[10];
+        Arrays.fill(myArr, "Assigned");
+        for(String str:myArr){
+            System.out.println(str);
+        }
+    }
+}
